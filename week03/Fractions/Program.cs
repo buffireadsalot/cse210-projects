1273f6a79bd9b3bbc6f7fb01f71081390b5f7dda
buffireadsalot <annelabuffin@gmail.com>
2025-01-22 12:56:24 -0500

    updated with proper responses.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -8,13 +8,17 @@ class Program
         Console.WriteLine(f.GetFractionString());
         Console.WriteLine(f.GetDecimalValue());
 
-        Fraction f1 = new Fraction(7);
+        Fraction f1 = new Fraction(5);
         Console.WriteLine(f1.GetFractionString());
         Console.WriteLine(f1.GetDecimalValue());
 
-        Fraction f2 = new Fraction(2,3);
+        Fraction f2 = new Fraction(3,4);
         Console.WriteLine(f2.GetFractionString());
         Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(1,3);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
         
     }
 }
