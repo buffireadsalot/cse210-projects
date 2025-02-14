880181effb0db72070523d66d6f32a482078d471
buffireadsalot <annelabuffin@gmail.com>
2025-02-14 13:30:27 -0500

    Exercise Tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
+// Main Program
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("14 Feb 2025", 30, 3.0),
+            new Cycling("14 Feb 2025", 30, 12.0),
+            new Swimming("14 Feb 2025", 30, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
